2c42baec1ec684262ffe95065c651e56d6b890ac
ARK <Arrkaddiy@gmail.com>
2020-09-06 20:31:36 +0300

    add entity CellName

---
+++ src/main/java/ru/my/home/entity/CellName.java
@@ -0,0 +1,22 @@
+package ru.my.home.entity;
+
+import ru.my.home.constatnt.ColumnName;
+
+public class CellName {
+
+    private final int rowIndex;
+    private final ColumnName columnName;
+
+    public CellName(int rowIndex, ColumnName columnName) {
+        this.rowIndex = rowIndex;
+        this.columnName = columnName;
+    }
+
+    public int getRowIndex() {
+        return rowIndex;
+    }
+
+    public ColumnName getColumnName() {
+        return columnName;
+    }
+}
